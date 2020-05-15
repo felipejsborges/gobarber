718539dcacb493bac09bec0a484f74e030e8022f
Felipe Borges <felipejsborges@outlook.com>
2020-05-14 21:39:12 -0300

    list providers feature

--- backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -8,12 +8,6 @@ const appointmentsController = new AppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
-// creating a route to list all appointments:
-// appointmentsRouter.get('/', async (request, response) => {
-// 	const appointments = await appointmentsRepository.find(); // find function with no params returns all that has on the repository
-// 	return response.json(appointments);
-// });
-
 appointmentsRouter.post('/', appointmentsController.create);
 
 export default appointmentsRouter;
