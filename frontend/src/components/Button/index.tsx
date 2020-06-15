736820aea887bc85c71ed447e1016128b552ae11
Felipe Borges <felipejsborges@outlook.com>
2020-06-15 20:44:21 -0300

    password recover on website

--- frontend/src/components/Button/index.tsx
+++ frontend/src/components/Button/index.tsx
@@ -2,12 +2,14 @@ import React, { ButtonHTMLAttributes } from 'react';
 
 import { Container } from './styles';
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>; // its the same of interface ButtonProps extends ButtonHTMLAttributes<HTMLInputElement> {}
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  loading?: boolean;
+}; // its the same of interface ButtonProps extends ButtonHTMLAttributes<HTMLInputElement> {}
 
-const Button: React.FC<ButtonProps> = ({ children, ...rest }) => (
+const Button: React.FC<ButtonProps> = ({ children, loading, ...rest }) => (
   <Container type="button" {...rest}>
     {/* a button alwais need type="button", but if I change it inside my page components, I change this original type="button" */}
-    {children}
+    {loading ? 'Carregando...' : children}
   </Container>
 );
 
