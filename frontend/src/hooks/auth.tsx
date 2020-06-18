43fc5bb829b8a70bd15e2d65e341c394704f7277
Felipe Borges <felipejsborges@outlook.com>
2020-06-17 21:43:12 -0300

    dashboard page created

--- frontend/src/hooks/auth.tsx
+++ frontend/src/hooks/auth.tsx
@@ -1,9 +1,14 @@
 import React, { createContext, useCallback, useState, useContext } from 'react';
 import api from '../services/api';
 
+interface User {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 
 interface SignInCretentials {
@@ -12,7 +17,7 @@ interface SignInCretentials {
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   signIn(credentials: SignInCretentials): Promise<void>;
   signOut(): void;
 }
@@ -25,6 +30,7 @@ const AuthProvider: React.FC = ({ children }) => {
     const user = localStorage.getItem('@GoBarber:user');
 
     if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
       return { token, user: JSON.parse(user) };
     }
 
@@ -40,6 +46,10 @@ const AuthProvider: React.FC = ({ children }) => {
     const { token, user } = response.data;
     localStorage.setItem('@GoBarber:token', token);
     localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+    // setting to send the token as headers.authorization in every api request
+    api.defaults.headers.authorization = `Bearer ${token}`;
+
     setData({ token, user });
   }, []);
 
